12d71576e615c6a3e2d9eaed368273ad530e2af0
Sneha-19 <19snehasingh97@gmail.com>
2025-05-22 14:27:35 +0530

    Authentication check for access to other pages

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,39 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import NavBar from "./NavBar"
 import Footer from "./Footer"
+import axios from "axios"
+import { BASE_URL } from "../utils/constants"
+import { useDispatch, useSelector } from "react-redux"
+import { addUser } from "../utils/userSlice"
+import { useEffect } from "react"
 
 const Body = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector(state => state.user)
+
+    const fetchUser = async () => {
+        if(user) return;
+        try {
+            const loggedInUser = await axios.get(BASE_URL + "/profile", { withCredentials: true });
+            dispatch(addUser(loggedInUser.data));
+        } catch (err) {
+            if (err.status === 401) {
+                navigate("/login")
+            }
+            console.error(err)
+        }
+    }
+
+    useEffect(() => {
+        fetchUser();
+    }, [])
+
     return (
         <>
-        <NavBar/>
-        <Outlet/>
-        <Footer/>
+            <NavBar />
+            <Outlet />
+            <Footer />
         </>
     )
 }
